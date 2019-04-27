15dfd64526c69d37d1bb4f4b93ec6e2a12f046c3
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2019-04-27 16:42:17 -0500

    Documentation for withHandlePressBack

--- app/src/navigation/handle-press-back.js
+++ app/src/navigation/handle-press-back.js
@@ -4,6 +4,11 @@ import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
 import getCurrentRoute from './get-current-route'
 
+/**
+ * Permite que un componente maneje el evento de atras en android
+ * @param WrappedComponent Componente que manejara el evento
+ * @param {Function} mapStateToProps Funcion que conecta WrappedComponent al store de redux
+ */
 const withHandlePressBack = (WrappedComponent, mapStateToProps) => connect(mapStateToProps)(
   class extends React.Component {
 
@@ -25,6 +30,7 @@ const withHandlePressBack = (WrappedComponent, mapStateToProps) => connect(mapSt
         this.timer = setTimeout(() => {this.counter = 0}, 500)
         this.counter += 1
         if (this.counter >= 2){
+          clearTimeout(this.timer)
           BackHandler.exitApp()
         }
       }
