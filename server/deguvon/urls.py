8b32a6201c4b4f0f01259a09c2c09ef3df8db7d9
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2019-08-05 18:37:53 -0500

    Renamed views in url

--- server/deguvon/urls.py
+++ server/deguvon/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
-from api import views
+from api import views as api_views
 
 router = routers.DefaultRouter()
-router.register(r'animes', views.AnimeViewSet)
-router.register(r'genres', views.GenreViewSet)
-router.register(r'states', views.StateViewSet)
-router.register(r'types', views.TypeViewSet)
-router.register(r'recents', views.RecentsViewSet, base_name='recents')
-router.register(r'directory', views.DirectoryViewSet, base_name='directory')
+router.register(r'animes', api_views.AnimeViewSet)
+router.register(r'genres', api_views.GenreViewSet)
+router.register(r'states', api_views.StateViewSet)
+router.register(r'types', api_views.TypeViewSet)
+router.register(r'recents', api_views.RecentsViewSet, base_name='recents')
+router.register(r'directory', api_views.DirectoryViewSet, base_name='directory')
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
