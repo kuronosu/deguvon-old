a2ca1dc16a0e5a755865a371183107749285e7b9
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2019-08-24 02:00:14 -0500

    url names in plural

--- server/deguvon/urls.py
+++ server/deguvon/urls.py
@@ -6,10 +6,10 @@ from api import views as api_views
 
 api_router = routers.DefaultRouter()
 api_router.APIRootView.__doc__ = "The root for Deguvon api"
-api_router.register(r'anime', api_views.AnimeViewSet)
-api_router.register(r'genre', api_views.GenreViewSet)
-api_router.register(r'state', api_views.StateViewSet)
-api_router.register(r'type', api_views.TypeViewSet)
+api_router.register(r'animes', api_views.AnimeViewSet)
+api_router.register(r'genres', api_views.GenreViewSet)
+api_router.register(r'states', api_views.StateViewSet)
+api_router.register(r'types', api_views.TypeViewSet)
 api_router.register(r'recents', api_views.RecentsViewSet, base_name='recents')
 api_router.register(r'directory', api_views.DirectoryViewSet,
                     base_name='directory')
