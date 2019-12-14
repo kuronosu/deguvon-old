bd89da617432f7ddad29d63d06841a80246796c5
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2019-12-14 03:31:54 -0500

    aid field changed to small int

--- server/api/models.py
+++ server/api/models.py
@@ -31,6 +31,7 @@ class Genre(models.Model):
 
 class Anime(models.Model):
     aid = models.CharField(unique=True, max_length=5, verbose_name=_("Anime ID"))
+    aid = models.PositiveSmallIntegerField()
     name = models.CharField(max_length=300, unique=True,
                             verbose_name=_('Nombre'))
     slug = models.SlugField(max_length=300, unique=True,
