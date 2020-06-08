535b8b5d968d40049dbfe3cd7ccb80cb01a787c4
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2020-06-07 19:57:43 -0500

    server to separated repository

--- server/api/pagination.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class AnimeSetPagination(PageNumberPagination):
-    page_size = 24
-    page_size_query_param = 'page_size'
-    max_page_size = 24
